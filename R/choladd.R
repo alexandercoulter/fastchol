9f9bc3493f7d0f7f015940757ea3cb13b27c8c22
alexandercoulter <coultera@tamu.edu>
2025-12-24 13:50:52 -0800

    Add R wrapper for choladd which allows user-specification of lower or upper.

---
+++ R/choladd.R
@@ -0,0 +1,33 @@
+#' Cholesky Row/Column Addition
+#'
+#' @description
+#' This function calculates the lower/upper Cholesky factor of positive definite
+#' matrix `M`, provided the Cholesky factor of `M[-k, -k]` is known. That is,
+#' this function inverts `choldrop`, re-inserting the `k`^th row/column from
+#' `M`.
+#' 
+#' @param CF Cholesky factor, either lower-triangular (in which case set
+#' `lower = TRUE`) or upper-triangular (in which case set `upper = FALSE`)
+#' @param z The "missing" row/column of `M`, i.e. any `p + 1`-long vector which,
+#' upon being inserted to be the `k`-th row/column in expanded matrix `M`,
+#' retains positive definiteness `M > 0`
+#' @param k The row/column index which `z`'s insertion will correspond to,
+#' from `1` to `p + 1` inclusive
+#' @param lower Boolean, dictating whether `CF` is the lower Cholesky factor
+#' (`TRUE`, default) or upper Cholesky factor (`FASLE`). Also dictates whether
+#' the returned Choleskyfactor is lower- or upper-triangular.
+#'
+#' @returns Cholesky factor of matrix `M`, where `M` is constructed from
+#' `M[-k, -k]` and `z` inserted at the `k`-th row/column index.
+#' @export
+#' @import Rcpp, RcppArmadillo
+#' 
+#' @examples
+choladd = function(CF, z, k, lower = TRUE){
+  
+  p1 = length(z)
+  R = matrix(0, p1, p1)
+  if(lower) choladdL_Rcpp(R, CF, z, k) else choladdU_Rcpp(R, CF, z, k)
+  R
+  
+}
