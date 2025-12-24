6b9e27b6661aba031764d9017e90dc3fd129e939
alexandercoulter <coultera@tamu.edu>
2025-12-24 07:29:33 -0800

    Create first function, cholupL, which conducts rank-1 Cholesky update using lower Cholesky factor, L.

---
+++ R/cholupL.R
@@ -0,0 +1,21 @@
+#' Cholesky Rank-1 Update Using Lower Cholesky Factor L
+#'
+#' @param L Lower-Cholesky factor of dimension `p` \eqn{\times} `p`
+#' @param x Any `p`-long vector
+#'
+#' @returns Matrix `M = L + xx'`
+#' @export
+#' @import Rcpp, RcppArmadillo
+#'
+#' @examples
+cholupL = function(L, x){
+  
+  p = length(x)
+  L0 = L
+  L0[1] = L0[1] + 0
+  x0 = x
+  x0[1] = x0[1] + 0
+  cholupL_Rcpp(L0, x0)
+  L0
+  
+}
