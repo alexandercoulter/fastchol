11fcecf0ac4929bd7016bfe5b2663b43b5efb9fa
alexandercoulter <coultera@tamu.edu>
2025-12-29 15:27:36 -0800

    Update description of rank-K update function, along with associated documentation.

--- R/cholupK.R
+++ R/cholupK.R
@@ -1,14 +1,14 @@
 #' Cholesky Rank-K Update
 #'
 #' @description
-#' This function calculates the rank-1 update of Cholesky factor `CF`, e.g.
-#' the Cholesky factor of matrix `(CF)(CF') + xx'`. Here, `CF` is a square `p`
-#' \eqn{\times} `p` Cholesky factor, and `x` is any arbitrary vector of length
-#' `p`. The user may specify whether `CF` is lower-triangular (in which case
-#' `lower = TRUE`) or upper-triangular (in which case `lower = FALSE`). This
-#' function applies Givens rotations to triangularize the joint matrix `(CF x)`;
-#' due to cache latency, it will generally be faster when `CF` is the lower
-#' Cholesky factor.
+#' This function calculates the rank-K update of Cholesky factor `CF`, e.g.
+#' the Cholesky factor of matrix `(CF)(CF') + XX'`. Here, `CF` is a square `p`
+#' \eqn{\times} `p` Cholesky factor, and `X` is any arbitrary matrix with `p`
+#' rows and `K` columns The user may specify whether `CF` is lower-triangular
+#' (in which case `lower = TRUE`) or upper-triangular (in which case
+#' `lower = FALSE`). This function applies Givens rotations to triangularize the
+#' joint matrix `(CF x)`; due to cache latency, it will generally be faster when
+#' `CF` is the lower Cholesky factor.
 #' 
 #' @param CF Cholesky factor of dimension `p` \eqn{\times} `p`
 #' @param X Any `p` \eqn{\times} `K` matrix
