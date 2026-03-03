4631083d69a912b971ec59890472b3cea9fa6d3e
alexandercoulter <coultera@tamu.edu>
2026-03-03 10:58:29 -0600

    Update trisolve function to accept matrix arguments.

--- R/trisolve.R
+++ R/trisolve.R
@@ -1,42 +1,53 @@
 #' Triangular Solver
 #'
 #' @description
-#' This function is a custom implementation of the default execution of 
-#' `forwardsolve` (when `lower = TRUE`) or `backsolve` (when `lower = FALSE`),
-#' evaluating \eqn{(CF)^{-1}x}.
+#' This function is a custom implementation of the base `R` functions 
+#' `forwardsolve` (when `lower = TRUE`) and `backsolve` (when `lower = FALSE`),
+#' evaluating \eqn{(CF)^{-1}X}.
 #'
-#' @param CF Triangular matrix of dimension `p` \eqn{\times} `p`
-#' @param x Any vector of length `p`.
+#' @param CF Triangular matrix of dimension `n` \eqn{\times} `n`
+#' @param X Any matrix of dimension `n` \eqn{\times} `p`, or vector of length `n`
 #' @param lower Boolean, dictating whether `CF` is lower-triangular
 #' (`TRUE`, default) or upper-triangular (`FASLE`).
 #'
-#' @returns Solution for `y` in linear system `(CF)y = x`, i.e. \eqn{(CF)^{-1}x}
+#' @returns Solution for `Y` in linear system `(CF)Y = X`, i.e. \eqn{(CF)^{-1}X}
 #' @export
 #'
 #' @examples
 #' # Generate positive definite matrix
-#' p = 20
-#' M = diag(p) + tcrossprod(matrix(rnorm(p * 2), p, 2))
+#' n = 20
+#' M = diag(n) + tcrossprod(matrix(rnorm(n * 2), n, 2))
 #' 
 #' # Calculate Cholesky factors
 #' U = chol(M)
 #' L = t(U)
 #' 
-#' # Generate vector for linear system
-#' x = rnorm(p)
+#' # Generate RHS matrix for linear system
+#' p = 5
+#' X = matrix(rnorm(n * p), n, p)
 #' 
 #' # Calculate triangular solves
-#' Lx = trisolve(L, x, lower = TRUE)
-#' Ux = trisolve(U, x, lower = FALSE)
+#' Lx = trisolve(L, X, lower = TRUE)
+#' Ux = trisolve(U, X, lower = FALSE)
 #' 
 #' # Compare to forwardsolve and backsolve
-#' max(abs(Lx - forwardsolve(L, x)))
-#' max(abs(Ux - backsolve(U, x)))
-trisolve = function(CF, x, lower = TRUE){
+#' max(abs(Lx - forwardsolve(L, X)))
+#' max(abs(Ux - backsolve(U, X)))
+trisolve = function(CF, X, lower = TRUE){
   
-  x0 = x
-  x0[1] = x0[1] + 0
-  if(lower) Lsolve_Rcpp(CF, x0) else Usolve_Rcpp(CF, x0)
-  x0
+  X0 = X
+  X0[1] = X0[1] + 0
+  
+  if(is.matrix(X0)){
+    
+    if(lower) LsolveX_Rcpp(CF, X0) else UsolveX_Rcpp(CF, X0)
+    
+  } else {
+    
+    if(lower) Lsolvex_Rcpp(CF, X0) else Usolvex_Rcpp(CF, X0)
+    
+  }
+  
+  X0
   
 }
